ec35de3c4f5ef9cf39fc6bf87ba80775c1ab3321
João Pedro <jotapedro1231@gmail.com>
2023-08-02 16:14:20 -0300

    Adicionando stitches

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
+import { getCssText } from "@/styles";
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
       <Head>
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;500;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
