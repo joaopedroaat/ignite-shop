c7e1e38dea6d8d32bbb824b0a22d5120d8256ce9
João Pedro <jotapedro1231@gmail.com>
2023-08-04 15:11:41 -0300

    Navigating between pages using next/link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import Stripe from 'stripe'
 
 import 'keen-slider/keen-slider.min.css'
+import Link from 'next/link'
 
 
 interface HomeProps {
@@ -30,13 +31,18 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className='keen-slider'>
       {products.map(product => {
         return (
-          <Product key={product.id} className='keen-slider__slide'>
-            <Image src={product.imageUrl} width={520} height={400} alt='' />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link 
+            key={product.id} 
+            href={`/product/${product.id}`}
+          >
+            <Product className='keen-slider__slide'>
+              <Image src={product.imageUrl} width={520} height={400} alt='' />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
