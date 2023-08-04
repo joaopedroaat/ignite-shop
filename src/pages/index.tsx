441cc38c9503fbd22f11dfe09321528bb001876e
João Pedro <jotapedro1231@gmail.com>
2023-08-04 14:55:55 -0300

    Creating SSG version of home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
+import { stripe } from '@/lib/stripe'
 import { HomeContainer, Product } from '@/styles/pages/home'
 import { useKeenSlider } from 'keen-slider/react'
+import { GetStaticProps } from 'next'
 import Image from 'next/image'
+import Stripe from 'stripe'
 
 import 'keen-slider/keen-slider.min.css'
 
-import { stripe } from '@/lib/stripe'
-import { GetServerSideProps } from 'next'
-import Stripe from 'stripe'
 
 interface HomeProps {
   products: {
@@ -43,7 +43,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -62,6 +62,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2, // 2 Hours
   }
 }
