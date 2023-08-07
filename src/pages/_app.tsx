0a12b8b673c4a9f2b1165d9e283c2f6217b91c36
João Pedro <jotapedro1231@gmail.com>
2023-08-07 16:08:04 -0300

    Added shopping cart button on header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,21 @@
-import { globalStyles } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app'
+import { Handbag } from '@phosphor-icons/react'
 import type { AppProps } from 'next/app'
 
-globalStyles()
-
 import logoImg from '@/assets/logo.svg'
-import { Container, Header } from '@/styles/pages/app'
 import Image from 'next/image'
+
+import { globalStyles } from '@/styles/global'
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
         <Image src={logoImg} alt="" />
+        <button>
+          <Handbag size={32} weight='bold' />
+        </button>
       </Header>
       <Component {...pageProps} />
     </Container>
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -12,5 +12,22 @@ export const Header = styled('header', {
   padding: '2rem 0',
   width: '100%',
   maxWidth: 1100,
-  margin: '0 auto'
+  margin: '0 auto',
+
+  display: 'flex',
+  justifyContent: 'space-between',
+
+  button: {
+    background: '$gray800',
+    maxWidth: 48,
+    maxHeight: 48,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '$gray500',
+    padding: '0.75rem',
+    cursor: 'pointer',
+    border: 'none',
+    borderRadius: 6,
+  }
 })
